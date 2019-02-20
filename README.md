1b39bf9da915672ff44889e857b8eb21b7cc7489
Johannes Frandsen <jsf@greenoak.dk>
2019-02-20 12:20:01 +0100

    Switched to vuejs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.org/Mathmagicians/mathmagicians.github.io.svg?branch=master)](https://travis-ci.org/Mathmagicians/mathmagicians.github.io)
+
 # mathmagicians.github.io
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<b-jumbotron header="Mathmagicians.dk" lead="Site is not live yet">
+  <v-icon name="atom" scale="10" />
+</b-jumbotron>
+</template>
+
+<script>
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/atom'
+
+export default {
+  name: 'home',
+  components: {
+    'v-icon': Icon
+  }
+}
+</script>
+
+<style scoped>
+</style>
