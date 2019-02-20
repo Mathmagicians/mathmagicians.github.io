9bde8abd493581c4329b7e21aa6acd53f8ad9a49
Travis <travis@travis-ci.org>
2019-02-20 15:07:23 +0000

    Deployed build 4 with travis

---
+++ js/chunk-2d22d746.e78b745f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("b-container")},o=[],u={name:"about"},a=u,i=t("2877"),r=Object(i["a"])(a,c,o,!1,null,"6f8cf5dc",null);r.options.__file="About.vue";e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.e78b745f.js.map
